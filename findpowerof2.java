3fed395376e3af60ef0b79d77b4d2e32bdf8c2fc
mounikapujari <mounikapujari2005@gmail.com>
2024-12-04 14:08:04 +0530

    Add files via upload

---
+++ findpowerof2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class findpowerof2{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+            System.out.println(1<<n);
+    }
+}
