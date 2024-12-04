d595c41de5e7a5dad5ae193b91aa828931593db6
mounikapujari <mounikapujari2005@gmail.com>
2024-12-04 12:24:20 +0530

    Add files via upload

---
+++ powerof2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class powerof2{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        if((a&(a-1))==0)
+        {
+            System.out.println("power of 2");
+        }
+        else
+        {
+            System.out.println("not power of 2");
+        }
+    }
+}
